550893deedec133a8369fc2100f3882bb116a73c
TakkAlGhul <takuro.mike@gmail.com>
2019-06-06 22:56:02 +0900

    Updates

---
+++ precache-manifest.07f67afa96cf45ea8493e77f944574c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37da9680f4fa6071d985dfcb17aa7d84",
+    "url": "/tk.portfolio/index.html"
+  },
+  {
+    "revision": "b92e81f2fc54c68382cd",
+    "url": "/tk.portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "3ce320f6122c8bf7b75e",
+    "url": "/tk.portfolio/static/css/main.4560e355.chunk.css"
+  },
+  {
+    "revision": "b92e81f2fc54c68382cd",
+    "url": "/tk.portfolio/static/js/2.7f6e9a07.chunk.js"
+  },
+  {
+    "revision": "3ce320f6122c8bf7b75e",
+    "url": "/tk.portfolio/static/js/main.d09f3f6a.chunk.js"
+  },
+  {
+    "revision": "d711074c20329186163b",
+    "url": "/tk.portfolio/static/js/runtime~main.171cb966.js"
+  }
+]);
