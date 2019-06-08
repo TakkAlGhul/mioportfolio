10a9640be8e1fd5f91dc126c8692da4764f67971
TakkAlGhul <takuro.mike@gmail.com>
2019-06-08 20:39:11 +0900

    Updates

---
+++ precache-manifest.6764407c369b018ff92fa5c305ea9b78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad0b825f55ec877ea9cf33813f944c15",
+    "url": "/tk-portfolio/index.html"
+  },
+  {
+    "revision": "9f3ea8b474594d8e99a5",
+    "url": "/tk-portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "a2fba813f83fe26163ad",
+    "url": "/tk-portfolio/static/css/main.6e711524.chunk.css"
+  },
+  {
+    "revision": "9f3ea8b474594d8e99a5",
+    "url": "/tk-portfolio/static/js/2.101a2feb.chunk.js"
+  },
+  {
+    "revision": "a2fba813f83fe26163ad",
+    "url": "/tk-portfolio/static/js/main.65fa8a90.chunk.js"
+  },
+  {
+    "revision": "fc55af8f0c864d898480",
+    "url": "/tk-portfolio/static/js/runtime~main.51af9058.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tk.portfolio/precache-manifest.07f67afa96cf45ea8493e77f944574c5.js"
+  "/tk-portfolio/precache-manifest.6764407c369b018ff92fa5c305ea9b78.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tk.portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tk-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
